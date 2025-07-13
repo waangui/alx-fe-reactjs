d99df9793c2024ca7ebba10b731fb3b5daa3ad70
Wangui Githua <barb.wangui@gmail.com>
2025-07-13 13:34:03 -0400

    Welcome message in components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WelcomeMessage /> 
     </>
   )
 }
