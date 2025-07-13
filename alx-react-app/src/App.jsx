448b6d66eab73ef828b4cdcd70fb272cd7fd8e4e
Wangui Githua <barb.wangui@gmail.com>
2025-07-13 13:42:25 -0400

    Added components: Header, Footer & Main content

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -30,6 +34,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage /> 
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
