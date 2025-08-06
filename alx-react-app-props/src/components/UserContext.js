607de6078b3f895140a8331408f5600ea0792772
Wangui Githua <barb.wangui@gmail.com>
2025-08-06 14:54:28 -0500

    Created user context and initialised it

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,5 +1,22 @@
-import {createContext, useContext} from 'react';
+import {Children, createContext, useContext} from 'react';
 
 const UserContext = createContext();
 
-export function UserProvider({})
+export function UserProvider({}) {
+    const userData = {
+        name: "Jane Doe",
+        email: "jane.doe@example.com"
+    };
+
+    return (
+        <UserContext.Provider value={userData}>
+            { Children }
+        </UserContext.Provider>
+    );
+}
+
+export function useUser (){
+    return useContext(UserContext);
+}
+
+export default UserContext;
