25df47713e4488aad29dfd6bd01915b5f0059b2f
Wangui Githua <barb.wangui@gmail.com>
2025-08-06 15:49:10 -0500

    moved content from UserDetails to UserProfile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,21 @@
-const UserProfile = (props) => {
-    return(
-        <div style={{padding:"15px", backgroundColor: "#EDF1E9", border: '0px', margin: '10px', borderStyle: 'solid'  }}>
-            <h2 style = {{ color: "#527125", fontWeight: "bold", fontSize:'26px'}}>{props.name}</h2>
-            <p style={{fontSize: '18px'}}> <span style={{fontWeight: 'bold', color: 'blue'}}>Age:</span> {props.age} </p>
-            <p style={{fontSize: '18px'}}> <span style={{fontWeight: 'bold', color: 'blue'}}>Bio:</span> {props.bio}</p>
-        </div>
-    )
+import {useContext} from 'react';
+import { UserContext} from './UserContext';
+
+
+
+function UserDetails() {
+
+    const userData = useContext(UserContext);
+
+    return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  ); 
 }
 
-export default UserProfile;
+  
+
+
+export default UserDetails;
