3bf58c77142cf8a06c8916a4e5b479273fa2e9a2
Wangui Githua <barb.wangui@gmail.com>
2025-08-06 17:33:02 -0500

    Justified content in navbar and used display:flex

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {Link } from 'react-router-dom';
 function Navbar (){
     return(
 
-        <nav style={{padding: '10px', textAlign:'right', fontSize:'18px', fontWeight:'bold', backgroundColor:'#EDF1E9', color: 'white'}}>
+        <nav style={{padding: '10px', textAlign:'right', fontSize:'18px', fontWeight:'bold', display:'flex', justifyContent:'right', backgroundColor:'#EDF1E9'}}>
             <Link to="/" style={{padding:'8px'}}>Home</Link>
             <Link to="/about" style={{padding:'8px'}}>About</Link>
             <Link to="/services" style={{padding:'8px'}}>Services</Link>
