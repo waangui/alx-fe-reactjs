a5729cdb5cf795da2c1473095a8363849822f719
Wangui Githua <barb.wangui@gmail.com>
2025-08-06 15:44:44 -0500

    Modified UserDetails.jsx to use context

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,10 +1,21 @@
-function UserDetails({ userData }) {
-  return (
+import {useContext} from 'react';
+import { UserContext} from './UserContext';
+
+
+
+function UserDetails() {
+
+    const userData = useContext(UserContext);
+
+    return (
     <div>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
-  );
+  ); 
 }
+
+  
+
 
 export default UserDetails;
